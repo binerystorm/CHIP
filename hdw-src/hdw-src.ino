07185c9715022128e715a663b4e14203561ef1fd
binerystorm <gerick.de.kruyf123@gmail.com>
2023-09-06 10:24:37 +0200

    ammendment to previuos commit

--- hdw-src/hdw-src.ino
+++ hdw-src/hdw-src.ino
@@ -1,6 +1,7 @@
-#include "chip/chip.hpp"
-#include "control/control.hpp"
-#include "tick/tick.hpp"
+#include "chip.hpp"
+#include "trace.h"
+#include "control.hpp"
+#include "tick.hpp"
 
 Type data[DATACAP] = {0};
 size_t caps[4] = {CHIPCAP, GLUE1CAP, WIRECAP, GLUE2CAP};
@@ -11,12 +12,15 @@ Control control = control_init(3, 4, A1);
 Clock clk = clock_init(0.4f);
 size_t tick_counter = 0;
 bool frame_updated = false;
+// Trace chip_trace = {0};
 
 void setup()
 {
     FastLED.addLeds<NEOPIXEL, 6>(data, DATACAP);
     pinMode(A1, INPUT);
     chip_init(&chip, caps, data, DATACAP);
+    // circuit_init(&chip_trace, {WIRECAP}, 1, chip.wire.data);
+
 }
 
 void loop()
@@ -32,6 +36,15 @@ void loop()
         sec_fill_gradient(&chip.glue1);
         sec_lerp_update_1d(&(control.power_state), &chip.glue2, 315);
         sec_fill_gradient(&chip.glue2);
+        sec_lerp_update(&((V2f){255,0}), &chip.wire, 315);
+        sec_fill(&(chip.wire));
         frame_updated = true;
     }
+    // if(tick_counter % 10 == 0){
+    //     if(chip_trace.pulsing)
+    //         trace_pulse(&chip_trace);
+    //     else{
+    //         if(random(map((int)control.power_state, 0,255, 25,0)) == 0) chip_trace.pulsing = true;
+    //     }
+    // }
 }
